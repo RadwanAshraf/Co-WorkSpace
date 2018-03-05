484e5d3a5bc46dc9ef723b2e10bac5cfaefd13e1
RadwanFCIS <radwan.ashraf.fcis@gmail.com>
2018-03-06 00:36:27 +0200

    Activities Created, added google-services.json, firebase dependencies

---
+++ app/src/main/java/com/radwan_pc/co_workspace/view/RoomsActivity.java
@@ -0,0 +1,15 @@
+package com.radwan_pc.co_workspace.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.radwan_pc.co_workspace.R;
+
+public class RoomsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rooms);
+    }
+}
